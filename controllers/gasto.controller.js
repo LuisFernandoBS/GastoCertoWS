1d4827b7ec40695bfcd003bfb739efc34010ea0a
LuisFernandoBS <luisfernando142536@gmail.com>
2024-09-22 16:18:39 -0300

    Adicinando o codigo da categoria ao insert e  update

--- controllers/gasto.controller.js
+++ controllers/gasto.controller.js
@@ -24,9 +24,9 @@ const gastoController = {
 
     create: async (req, res) => {
         try {
-            const { descricao, data_gasto, forma_pagamento, qtd_parcelas, status_pagamento, valor } = req.body;
-            const sql = 'INSERT INTO tb_gastos (descricao, data_gasto, forma_pagamento, qtd_parcelas, status_pagamento, valor) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
-            const { rows } = await postgre.query(sql, [descricao, data_gasto, forma_pagamento, qtd_parcelas, status_pagamento, valor]);
+            const { descricao, data_gasto, forma_pagamento, qtd_parcelas, status_pagamento, valor, cod_categoria } = req.body;
+            const sql = 'INSERT INTO tb_gastos (descricao, data_gasto, forma_pagamento, qtd_parcelas, status_pagamento, valor, cod_categoria) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *';
+            const { rows } = await postgre.query(sql, [descricao, data_gasto, forma_pagamento, qtd_parcelas, status_pagamento, valor, cod_categoria]);
             res.json({ msg: "success", data: rows[0] });
         } catch (error) {
             res.status(500).json({ msg: error.message });
@@ -35,9 +35,9 @@ const gastoController = {
 
     updateById: async (req, res) => {
         try {
-            const { descricao, data_gasto, forma_pagamento, qtd_parcelas, status_pagamento, valor } = req.body;
-            const sql = 'UPDATE tb_gastos SET descricao = $1, data_gasto = $2, forma_pagamento = $3, qtd_parcelas = $4, status_pagamento = $5, valor = $6 WHERE cod = $7 RETURNING *';
-            const { rows } = await postgre.query(sql, [descricao, data_gasto, forma_pagamento, qtd_parcelas, status_pagamento, valor, req.params.id]);
+            const { descricao, data_gasto, forma_pagamento, qtd_parcelas, status_pagamento, valor, cod_categoria } = req.body;
+            const sql = 'UPDATE tb_gastos SET descricao = $1, data_gasto = $2, forma_pagamento = $3, qtd_parcelas = $4, status_pagamento = $5, valor = $6, cod_categoria = $7 WHERE cod = $8 RETURNING *';
+            const { rows } = await postgre.query(sql, [descricao, data_gasto, forma_pagamento, qtd_parcelas, status_pagamento, valor, cod_categoria, req.params.id]);
             res.json({ msg: "success", data: rows[0] });
         } catch (error) {
             res.status(500).json({ msg: error.message });
